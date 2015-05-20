e42c82838575c88c21752d936c16f32855bd252c
Fernando santos <fernando.wdesign@icloud.com>
2015-05-19 22:46:23 -0300

    Intro PNLD 2014 criado em 2012

---
+++ intro_v07_edgeActions.js
@@ -0,0 +1,50 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",11000,function(sym,e){});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'linha1-animada'
+(function(symbolName){})("linha1-animada");
+//Edge symbol end:'linha1-animada'
+
+//=========================================================
+
+//Edge symbol: 'linha2-animada'
+(function(symbolName){})("linha2-animada");
+//Edge symbol end:'linha2-animada'
+
+//=========================================================
+
+//Edge symbol: 'linha3-animada'
+(function(symbolName){})("linha3-animada");
+//Edge symbol end:'linha3-animada'
+
+//=========================================================
+
+//Edge symbol: 'linha-animada5'
+(function(symbolName){})("linha-animada5");
+//Edge symbol end:'linha-animada5'
+
+//=========================================================
+
+//Edge symbol: 'linha-animada6'
+(function(symbolName){})("linha-animada6");
+//Edge symbol end:'linha-animada6'
+
+//=========================================================
+
+//Edge symbol: 'linha-animada7'
+(function(symbolName){})("linha-animada7");
+//Edge symbol end:'linha-animada7'
+
+//=========================================================
+
+//Edge symbol: 'linha4-animada'
+(function(symbolName){})("linha4-animada");
+//Edge symbol end:'linha4-animada'
+})(jQuery,AdobeEdge,"EDGE-3243554");
